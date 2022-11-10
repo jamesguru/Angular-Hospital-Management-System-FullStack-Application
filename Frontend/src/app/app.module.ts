f81d185feb28dccdc2e28ca17e84be137b8490d3
unknown <jameskagunga2022@gmail.com>
2022-11-10 03:22:26 -0800

    completed the help register page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { ResetComponent } from './components/reset/reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
