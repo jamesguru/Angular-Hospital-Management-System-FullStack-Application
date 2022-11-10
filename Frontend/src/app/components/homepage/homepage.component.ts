b4a9ab7c495c0b81ba72517b9c519cba9c94f1a0
unknown <jameskagunga2022@gmail.com>
2022-11-10 03:03:07 -0800

    worked on homepage

--- Frontend/src/app/components/homepage/homepage.component.ts
+++ Frontend/src/app/components/homepage/homepage.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  login: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+
+
+    setTimeout(()=>{
+
+      this.login=true;
+    },4000)
   }
 
+  showLogin(condition: string) {
+    if (condition === 'open') {
+      this.login = true;
+    } else {
+      this.login = false;
+    }
+  }
 }
